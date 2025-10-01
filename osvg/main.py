8a71e7b9fa51fae78eb707de46200266d84b3918
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-10-01 12:35:49 -0500

    Fix issue with how the many to many table was being created

--- osvg/main.py
+++ osvg/main.py
@@ -9,7 +9,8 @@ import sys
 from pathlib import Path
 
 import pandas
-from pandas import DataFrame
+from pandas import DataFrame, Series
+from pandas.core.groupby import DataFrameGroupBy
 from pydantic import BaseModel
 
 import osvg.types as osvg_types
@@ -39,27 +40,41 @@ def read_csv_file(filepath: Path, model: type[BaseModel]) -> DataFrame:
 def create_video_game_to_dataset_table(
     video_game_df: DataFrame,
     dataset_df: DataFrame,
-) -> DataFrame:
-    # Create an index of all datasets from the dataset DataFrame
-    dataset_url_to_index = dataset_df.index.to_series(index=dataset_df["url"].unique())
+) -> tuple[DataFrame, DataFrame]:
+    # Data structure to store information
+    data: dict[str, list[int]] = {"video_game_id": [], "dataset_id": []}
 
-    # Map the index to the dataset url in the video games DataFrame
-    vg_copy_df: DataFrame = video_game_df.copy()
-    vg_copy_df["dataset_id"] = vg_copy_df["dataset_url"].map(dataset_url_to_index)
+    # Create unique video games dataframe
+    unique_vg_df: DataFrame = video_game_df.copy().drop_duplicates(
+        subset="source_code_url"
+    )
 
-    # Extract the video game and dataset DataFrame indexes into columns
-    mapping: DataFrame = vg_copy_df[["dataset_id", "source_code_url"]].reset_index()
+    # Iterate through unique video game rows
+    idx: int
+    row: Series
+    for idx, row in unique_vg_df.iterrows():
+        # Get the video game source
+        vg_source: str = row["source_code_url"]
 
-    # Map each source code URL to its first instance `index` value
-    unique_mapping = mapping.groupby("source_code_url")["index"].transform("first")
+        # Find all instances of that video game in the original DataFrame
+        vg_specific_df: DataFrame = video_game_df[
+            video_game_df["source_code_url"] == vg_source
+        ]
 
-    # Replace the index column with the first occurrence of the index
-    mapping["index"] = unique_mapping
+        # Get all datasets associated with this video game
+        vg_datasets: Series[str] = vg_specific_df["dataset_url"]
 
-    # Rename columns
-    mapping = mapping.rename(columns={"index": "video_game_id"})
+        # Iterate through datasets
+        dataset: str
+        for dataset in vg_datasets:
+            # Lookup dataset ID in the dataset DataFrame
+            dataset_id: int = dataset_df[dataset_df["url"] == dataset].index[0]
 
-    return mapping[["video_game_id", "dataset_id"]].reset_index(drop=True)
+            # Append information to the data structure
+            data["video_game_id"].append(idx)
+            data["dataset_id"].append(dataset_id)
+
+    return (unique_vg_df, DataFrame(data=data))
 
 
 def main() -> None:
@@ -94,19 +109,20 @@ def main() -> None:
                 model=osvg_types.VideoGameDatasetsCSV,
             )
 
-            # Drop extrac columns
-            datasets_df = datasets_df.drop(columns="notes")
-
-            video_game_to_dataset_df: DataFrame = create_video_game_to_dataset_table(
+            # Create many to many DataFrame table
+            video_games_df, vg2ds_df = create_video_game_to_dataset_table(
                 video_game_df=video_games_df,
                 dataset_df=datasets_df,
             )
+
+            # Format DataFrames
+            datasets_df = datasets_df.drop(columns="notes")
 
             # Write data to tables
             db.write_df_to_table(df=datasets_df, table="datasets")
             db.write_df_to_table(df=video_games_df, table="video_games")
             db.write_df_to_table(
-                df=video_game_to_dataset_df,
+                df=vg2ds_df,
                 table="video_games_to_datasets",
             )
 
